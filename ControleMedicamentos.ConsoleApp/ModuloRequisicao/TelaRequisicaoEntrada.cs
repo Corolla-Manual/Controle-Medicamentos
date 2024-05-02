997f082090015aa361cb84618b30772af9c87da4
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-05-02 15:36:33 -0300

    at

---
+++ ControleMedicamentos.ConsoleApp/ModuloRequisicao/TelaRequisicaoEntrada.cs
@@ -0,0 +1,112 @@
+﻿using ControleMedicamentos.ConsoleApp.Compartilhado;
+using ControleMedicamentos.ConsoleApp.ModuloFornecedor;
+using ControleMedicamentos.ConsoleApp.ModuloFuncionario;
+using ControleMedicamentos.ConsoleApp.ModuloMedicamento;
+using ControleMedicamentos.ConsoleApp.ModuloPaciente;
+
+namespace ControleMedicamentos.ConsoleApp.ModuloRequisicao
+{
+    internal class TelaRequisicaoEntrada : TelaBase
+    {
+        public TelaMedicamento telaMedicamento = null;
+        public TelaFuncionario telaFuncionario = null;
+        public TelaFornecedor telaFornecedor = null;
+
+        public RepositorioFuncionario repositorioFuncionario = null;
+        public RepositorioMedicamento repositorioMedicamento = null;
+        public RepositorioFornecedor repositorioFornecedor = null;
+
+        public override void Registrar()
+        {
+            ApresentarCabecalho();
+
+            Console.WriteLine($"Cadastrando {tipoEntidade}");
+
+            Console.WriteLine();
+
+            RequisicaoEntrada entidade = (RequisicaoEntrada)ObterRegistro();
+
+            string[] erros = entidade.Validar();
+
+            if (erros.Length > 0)
+            {
+                ApresentarErros(erros);
+                return;
+            }
+
+            bool conseguiuRetirar = entidade.RetirarMedicamento();
+
+            if (!conseguiuRetirar)
+            {
+                ExibirMensagem("A quantidade de retirada informada não está disponível.", ConsoleColor.DarkYellow);
+                return;
+            }
+
+            repositorio.Cadastrar(entidade);
+
+            ExibirMensagem($"O {tipoEntidade} foi cadastrado com sucesso!", ConsoleColor.Green);
+        }
+
+        public override void VisualizarRegistros(bool exibirTitulo)
+        {
+            if (exibirTitulo)
+            {
+                ApresentarCabecalho();
+
+                Console.WriteLine("Visualizando Requisições de Entrada...");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(
+                "{0, -10} | {1, -15} | {2, -15} | {3, -20} | {4, -5}",
+                "Id", "Medicamento", "Paciente", "Data de Requisição", "Quantidade"
+            );
+
+            EntidadeBase[] requisicoesCadastradas = repositorio.SelecionarTodos();
+
+            foreach (RequisicaoSaida requisicao in requisicoesCadastradas)
+            {
+                if (requisicao == null)
+                    continue;
+
+                Console.WriteLine(
+                    "{0, -10} | {1, -15} | {2, -15} | {3, -20} | {4, -5}",
+                    requisicao.Id,
+                    requisicao.Medicamento.Nome,
+                    requisicao.Paciente.Nome,
+                    requisicao.DataRequisicao.ToShortDateString(),
+                    requisicao.QuantidadeRetirada
+                );
+            }
+
+            Console.ReadLine();
+            Console.WriteLine();
+        }
+
+        protected override EntidadeBase ObterRegistro()
+        {
+            telaMedicamento.VisualizarRegistros(false);
+
+            Console.Write("Digite o ID do medicamento requisitado: ");
+            int idMedicamento = Convert.ToInt32(Console.ReadLine());
+
+            Medicamento medicamentoSelecionado = (Medicamento)repositorioMedicamento.SelecionarPorId(idMedicamento);
+
+            telaPaciente.VisualizarRegistros(false);
+
+            Console.Write("Digite o ID do paciente requisitante: ");
+            int idPaciente = Convert.ToInt32(Console.ReadLine());
+
+            Paciente pacienteSelecionado = (Paciente)repositorioPaciente.SelecionarPorId(idPaciente);
+
+            Console.Write("Digite a quantidade do medicamente que deseja retirar: ");
+            int quantidade = Convert.ToInt32(Console.ReadLine());
+
+            RequisicaoEntrada novaRequisicao = new RequisicaoEntrada(medicamentoSelecionado, pacienteSelecionado, quantidade);
+
+            return novaRequisicao;
+        }
+
+    }
+}
